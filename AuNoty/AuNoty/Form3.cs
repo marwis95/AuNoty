38feac5dc57663e0d1a4e1a928c985362d6f1949
marwis95 <marwis95@o2.pl>
2018-03-13 11:48:58 +0100

    Encoding password

--- AuNoty/AuNoty/Form3.cs
+++ AuNoty/AuNoty/Form3.cs
@@ -11,15 +11,39 @@ namespace AuNoty
 {
     public partial class Form3 : Form
     {
+        public String tekst;
+
         public Form3()
         {
             InitializeComponent();
+        }
+
+        public String encode(String s)
+        {
+            int i=0;
+            int number=0;
+            foreach (char c in s)
+            {
+                StringBuilder sb = new StringBuilder(s);
+                number = Convert.ToInt32(sb[i]);
+                sb[i] = (char) (number+1);
+                s = sb.ToString();
+
+                i++;
+            }
+
+            return s;
         }
 
         private void Form3_Load(object sender, EventArgs e)
         {
             this.Location = new Point(Screen.PrimaryScreen.WorkingArea.Width - this.Width, Screen.PrimaryScreen.WorkingArea.Height - this.Height); 
             
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(encode("qwe"));
         }
     }
 }
