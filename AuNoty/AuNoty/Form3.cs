9b3643355d6f7d8a71cfd16c8fdee25653b126dd
marwis95 <marwis95@o2.pl>
2018-03-21 14:56:57 +0100

    Refactored Form3

--- AuNoty/AuNoty/Form3.cs
+++ AuNoty/AuNoty/Form3.cs
@@ -35,7 +35,7 @@ namespace AuNoty
             }
 
             return s;
-        }
+        } //kodowanie hasła
 
 
         public String decode(String s)
@@ -53,12 +53,12 @@ namespace AuNoty
             }
 
             return s;
-        }
+        } //dekodowanie hasła
 
         private void Form3_Load(object sender, EventArgs e)
         {
             try
-            {   
+            {//odczyt starego hasła z pliku   
                 using (StreamReader sr = new StreamReader("txt.txt"))
                 {
                     oldPass = sr.ReadToEnd();
@@ -72,22 +72,19 @@ namespace AuNoty
 
             oldPass = decode(oldPass);
             
-
-            //this.Location = new Point(Screen.PrimaryScreen.WorkingArea.Width - this.Width, Screen.PrimaryScreen.WorkingArea.Height - this.Height);
             this.Location = new Point((int)(Screen.PrimaryScreen.WorkingArea.Width * 0.5) - (int)(this.Width * 0.5), (int)(Screen.PrimaryScreen.WorkingArea.Height * 0.5) - (int)(this.Height * 0.5));
+           
             textBox1.UseSystemPasswordChar = PasswordPropertyTextAttribute.Yes.Password;
             textBox2.UseSystemPasswordChar = PasswordPropertyTextAttribute.Yes.Password;
             textBox3.UseSystemPasswordChar = PasswordPropertyTextAttribute.Yes.Password;
+            //nadawanie editom własciwości "password"
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //MessageBox.Show(encode("qwe"));
-            //MessageBox.Show(decode("qwe"));
-            //MessageBox.Show(decode(encode("qwe")));
 
             if ((textBox2.Text == textBox3.Text) && ((textBox1.Text == "Aumatic2018") || (textBox1.Text == oldPass)))
-            {
+            {//jeżeli zostanie podane stare hasło oraz nowe to możliwa jest zmiana
                 File.WriteAllText("txt.txt", encode(textBox3.Text));
                 MessageBox.Show("Password changed");
             }
@@ -101,12 +98,12 @@ namespace AuNoty
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
             if ((textBox2.Text == textBox3.Text) && (textBox2.Text != "") && (textBox3.Text != ""))
-            {
+            {//jeżeli wprowadzimy nowe hasło i poprawne powótrzenie to edity zmieniają kolor na zielony
                 textBox2.BackColor = System.Drawing.ColorTranslator.FromHtml("#29ba7b");
                 textBox3.BackColor = System.Drawing.ColorTranslator.FromHtml("#29ba7b");
             }
             else
-            {
+            {//w przeciwnym wypadku ich kolor staje sie standardowy
                 textBox2.BackColor = SystemColors.Window;
                 textBox3.BackColor = SystemColors.Window;
             }
@@ -116,19 +113,19 @@ namespace AuNoty
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
             if ((textBox2.Text == textBox3.Text) && (textBox2.Text != "") && (textBox3.Text != ""))
-            {
+            {//jeżeli wprowadzimy nowe hasło i poprawne powótrzenie to edity zmieniają kolor na zielony
                 textBox2.BackColor = System.Drawing.ColorTranslator.FromHtml("#29ba7b");
                 textBox3.BackColor = System.Drawing.ColorTranslator.FromHtml("#29ba7b");
             }
             else
-            {
+            {//w przeciwnym wypadku ich kolor staje sie standardowy
                 textBox2.BackColor = SystemColors.Window;
                 textBox3.BackColor = SystemColors.Window;
             }
         }
 
 
-
+        #region podglad_hasel
         private void button2_MouseDown(object sender, MouseEventArgs e)
         {
             textBox1.UseSystemPasswordChar = PasswordPropertyTextAttribute.No.Password;
@@ -159,15 +156,18 @@ namespace AuNoty
             textBox3.UseSystemPasswordChar = PasswordPropertyTextAttribute.Yes.Password;
         }
 
+        #endregion
+
+
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             if ((textBox1.Text == "Aumatic2018") || (textBox1.Text == oldPass))
-            {
+            {//jeżeli podamy stare hasło to zostanie odblokowana możliwość nadawania nowego
                 textBox2.Enabled = true;
                 textBox3.Enabled = true;
             }
             else
-            {
+            {//w przeciwnym wypadku nieaktywne
                 textBox2.Enabled = false;
                 textBox3.Enabled = false;
             }
